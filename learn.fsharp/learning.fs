4b10f97cd0379a628f4d5b9d331b41c38df97274
Daniel Williams <dlwiii@gmail.com>
2013-06-01 22:25:07 -0600

    intitial project

---
+++ learn.fsharp/learning.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main (args : string[]) =
+  if args.Length <> 2 then
+    failwith "error: expected two argumets"
+  let greeting, thing = args.[0], args.[1] 
+  printfn "Hello %s %s"  greeting thing
+  0 // return an integer exit code
